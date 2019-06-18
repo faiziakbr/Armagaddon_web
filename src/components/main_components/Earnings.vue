9a03134f38265ab0dabeaf0cca6ee26a58475374
faizan <faizi.akbr@gmail.com>
2019-06-18 12:00:35 +0500

    Earnings UI is changed but not approved

--- src/components/main_components/Earnings.vue
+++ src/components/main_components/Earnings.vue
@@ -1,13 +1,40 @@
 <template>
   <v-layout column>
-    <v-card>
-      <v-card-title
-        class="headline"
-      >Personal balance Rs.{{earnings.total_earned - earnings.withdrawn}}</v-card-title>
-    </v-card>
+    <v-layout row>
+      <v-flex>
+        <v-card class="mx-2 my-2">
+          <v-card-title>
+            <div>
+              <h3 class="headline">Personal Balance</h3>
+              <h3 class="headline">${{earnings.total_earned - earnings.withdrawn}}</h3>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+      <v-flex>
+        <v-card class="mx-2 my-2">
+          <v-card-title>
+            <div>
+              <h3 class="headline">Total Earned</h3>
+              <h3 class="headline">${{earnings.total_earned}}</h3>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+      <v-flex>
+        <v-card class="mx-2 my-2">
+          <v-card-title>
+            <div>
+              <h3 class="headline">Withdrawn</h3>
+              <h3 class="headline">${{earnings.withdrawn}}</h3>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
     <v-card-title>Transactions</v-card-title>
     <v-layout column>
-      <v-flex v-for="item in transactions" :key="item.id" xs12 class=" py-2 px-2">
+      <v-flex v-for="item in transactions" :key="item.id" xs12 class="py-2 px-2">
         <item-transaction :itemTransaction="item"></item-transaction>
       </v-flex>
     </v-layout>
@@ -33,10 +60,12 @@ export default {
     };
   },
   mounted() {
-    let user = JSON.parse(localStorage.getItem('user'));
+    let user = JSON.parse(localStorage.getItem("user"));
     axios({
       method: "GET",
-      url: "http://www.vacayplanet.com/ArmageddonApi/public/api/earnings/"+user.id
+      url:
+        "http://www.vacayplanet.com/ArmageddonApi/public/api/earnings/" +
+        user.id
     })
       .then(response => {
         this.earnings = response.data.earnings;
