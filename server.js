97dab3731a42101b779c605766ab702936c1ad20
faizan <faizi.akbr@gmail.com>
2019-06-23 21:26:10 +0500

    Adding files again

--- server.js
+++ server.js
@@ -3,18 +3,9 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-const history = require('connect-history-api-fallback');
 
 app = express();
-
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist/'));
-
-app.use(staticFileMiddleware);
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}));
-app.use(staticFileMiddleware);
+app.use(serveStatic(__dirname + "/dist"));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
