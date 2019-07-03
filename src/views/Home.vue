cf423489fe7a9502d01348a0df696fbbb4463e16
Muhammad Faizan <faizi.akbr@gmail.com>
2019-07-04 00:10:25 +0500

    Dashboard UI is set

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,49 @@
 <template>
-  <div>
-    <v-layout justify-center>
-      <h3 class="headline" style="font-weight:bold">Dashboard</h3>
+  <v-container fill-height fluid>
+    <v-layout row wrap align-center align-content-space-between>
+      <v-flex xs12>
+        <v-flex>
+          <h3 class="headline text-xs-center" style="font-weight:bold">Dashboard</h3>
+        </v-flex>
+        <v-layout row wrap>
+          <v-flex xs12 sm4>
+            <v-card class="mx-2 my-2">
+              <div class="pa-2">
+                <p class="grey--text title" style="text-align:center">Personal Balance</p>
+                <h4 class="display-1 text-xs-center">${{earnings.total_earned - earnings.withdrawn}}</h4>
+              </div>
+            </v-card>
+          </v-flex>
+          <v-flex xs12 sm4>
+            <v-card class="mx-2 my-2">
+              <div class="pa-2">
+                <p class="grey--text title" style="text-align:center">Referrals</p>
+                <h4 class="display-1" style="text-align:center">{{referrals}}</h4>
+              </div>
+            </v-card>
+          </v-flex>
+          <v-flex xs12 sm4>
+            <v-card class="mx-2 my-2">
+              <div class="pa-2">
+                <p class="grey--text title" style="text-align:center">Subscription</p>
+                <h4 class="display-1" style="text-align:center">{{days}} days left</h4>
+              </div>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 :class="{'text-xs-right':$vuetify.breakpoint.smAndUp, 'text-xs-center' : $vuetify.breakpoint.xs}">
+          <img :src="require('../assets/app_store.png')" width="200" height="70" contain />
+        </v-flex>
+        <v-flex xs12 sm6 :class="{'text-xs-left':$vuetify.breakpoint.smAndUp, 'text-xs-center' : $vuetify.breakpoint.xs}">
+          <img :src="require('../assets/play_store.png')" width="200" height="70" contain />
+        </v-flex>
+      </v-layout>
     </v-layout>
-    <v-layout row wrap>
-      <v-flex xs12 sm4>
-        <v-card class="mx-2 my-2">
-          <div class="pa-2">
-            <p class="grey--text title" style="text-align:center">Personal Balance</p>
-            <h3 class="display-2 text-xs-center">${{earnings.total_earned - earnings.withdrawn}}</h3>
-          </div>
-        </v-card>
-      </v-flex>
-      <v-flex xs12 sm4>
-        <v-card class="mx-2 my-2">
-          <div class="pa-2">
-            <p class="grey--text title" style="text-align:center">Referrals</p>
-            <h3 class="display-2" style="text-align:center">{{referrals}}</h3>
-          </div>
-        </v-card>
-      </v-flex>
-      <v-flex xs12 sm4>
-        <v-card class="mx-2 my-2">
-          <div class="pa-2">
-            <p class="grey--text title" style="text-align:center">Subscription</p>
-            <h3 class="display-2" style="text-align:center">{{days}} days left</h3>
-          </div>
-        </v-card>
-      </v-flex>
-    </v-layout>
-    <v-layout class="text-xs-center">
-      <v-flex>
-        <v-avatar :size="150">
-          <v-img :src="require('../assets/app_store.png')"></v-img>
-        </v-avatar>
-      </v-flex>
-      <v-flex>
-        <v-avatar :size="150">
-          <v-img :src="require('../assets/play_store.jpg')"></v-img>
-        </v-avatar>
-      </v-flex>
-    </v-layout>
+
     <my-loader :showDialog="loading"></my-loader>
-  </div>
+  </v-container>
 </template>
 
 <script>
